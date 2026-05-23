72a274efc9f3a1f0778b6ffb7c732126c328f8b8
Anas Reza <41887962+AnasReza31@users.noreply.github.com>
2026-05-23 21:26:39 +0530

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,14 +1,14 @@
 import requests, json
 
-def emotion_detector(textArg):
-    text_to_analyze = textArg
+def emotion_detector(text_to_analyze):
+
     api_url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(api_url, json = payload, headers=headers)
 
-    if response.status_code != 200:
+    if response.status_code == 400:
         return {
             'anger': None,
             'disgust': None,
@@ -17,7 +17,7 @@ def emotion_detector(textArg):
             'sadness': None,
             'dominant_emotion': None
         }
-    
+
     formatted_res = json.loads(response.text)        
     predictions = formatted_res.get("emotionPredictions")
     emotions = predictions[0]["emotion"] 
